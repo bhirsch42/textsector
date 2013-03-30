3b4db02c72e68df74a69fb6aa10bc71c25b9c9cb
Ben Hirsch <BenHirsch@Bens-MacBook-Pro.local>
2013-03-30 02:13:53 -0400

    Reorganized, images added

--- py/handler.py
+++ py/handler.py
@@ -5,42 +5,33 @@ import jinja2
 import logging
 import hashlib
 from google.appengine.ext import ndb
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
-
-cookie_secret = "fight_club"
-
-class User(ndb.Model):
-	username = ndb.StringProperty()
-	password = ndb.StringProperty()
+from utils import *
 
 class Handler(webapp2.RequestHandler):
 
+	# set up jinja workspace
+	template_dir = os.path.join(os.path.dirname(__file__), '../html')
+	jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+	# calls default initializer (syntax for future, does nothing yet)
  	def initialize(self, *a, **kw):
 		webapp2.RequestHandler.initialize(self, *a, **kw)
-		self.user_cookie_name = 'user_id'
-		uid = self.read_secure_cookie(self.user_cookie_name)
-		self.user = uid and User.get_by_id(int(uid))
 
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
+	# returns html from jinja template with parameters as a string
+	# takes template file name as first argument
 	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
+		t = jinja_env.get_template(template_dir+template)
 		return t.render(params)
 
+	# writes to webpage from jinja template
+	# takes template file name as first argument
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
-	def getDefaultHeader(self):
-		template = jinja_env.get_template('DefaultPageHeader.html')
-		if self.user:
-			return template.render(username=self.user.username)
-		else:
-			return template.render(username=None)
-
-
+	# add a hashed cookie to the user's browser
 	def set_secure_cookie(self, name, val, days = None):
 		cookie_val = make_secure_val(str(val))
 		if days == None:
@@ -49,24 +40,8 @@ class Handler(webapp2.RequestHandler):
 			expiration = datetime.datetime.now() + datetime.timedelta(days = days)
 			self.response.headers.add_header('Set-Cookie', '%s=%s; expires=%s; Path=/;' % (name, cookie_val, expiration.strftime("%a, %d-%b-%Y %H:%M:%S GMT")))
 
-
+	# returns value from user's browser cookie, or None if cookie is invalid
 	def read_secure_cookie(self, name):
 		cookie_val = self.request.cookies.get(name)
 		if cookie_val and check_secure_val(cookie_val):
 			return cookie_val.split('|')[0]
-
-	def render_default_header(self):
-		self.write(self.getDefaultHeader())
-
-
-def my_hash(val):
-	return str(hashlib.sha256(val).hexdigest())
-
-def make_secure_val(val):
-	return "%s|%s" % (val, my_hash(val + cookie_secret))
-
-def check_secure_val(secure_val):
-	val = secure_val.split('|')[0]
-	if secure_val == make_secure_val(val):
-		return val
-
---
+++ py/utils.py
@@ -0,0 +1,21 @@
+import webapp2
+import cgi
+import os
+import jinja2
+import logging
+import hashlib
+from google.appengine.ext import ndb
+
+cookie_secret = "fight_club"
+
+def my_hash(val):
+	return str(hashlib.sha256(val).hexdigest())
+
+def make_secure_val(val):
+	return "%s|%s" % (val, my_hash(val + cookie_secret))
+
+def check_secure_val(secure_val):
+	val = secure_val.split('|')[0]
+	if secure_val == make_secure_val(val):
+		return val
+
